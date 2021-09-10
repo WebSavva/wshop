d349d49ecbad48c1e8a51663c127863895a3eee3
WebSavva <70653174+WebSavva@users.noreply.github.com>
2021-09-11 01:09:10 +0300

    Small bugs in api and styles are fixed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ const getOrderById = asyncHandler(async (req, res) => {
 
     if (requestedOrder.user) {
       const orderOwner = requestedOrder.user.toString();
-      if (orderOwner !== req.user.id && !req.user.isAdmin)
+      if (orderOwner !== req.user.id || !req.user.isAdmin)
         res.status(403).json({
           message: "You are permitted to see the details of the given order",
         });
